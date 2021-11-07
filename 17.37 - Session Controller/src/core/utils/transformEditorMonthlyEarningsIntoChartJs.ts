c9eeca29889ed6b55cb196333821a27ad6f3a1cd
Daniel Bonifacio <contato@danielbonifacio.com.br>
2021-11-07 20:21:59 -0300

    fix date format

--- 17.37 - Session Controller/src/core/utils/transformEditorMonthlyEarningsIntoChartJs.ts
+++ 17.37 - Session Controller/src/core/utils/transformEditorMonthlyEarningsIntoChartJs.ts
@@ -1,5 +1,6 @@
 import { ChartProps } from "../../app/components/Chart/Chart";
-import { format } from "date-fns";
+import format from "date-fns/format";
+import parseISO from "date-fns/parseISO";
 import { ptBR } from "date-fns/locale";
 import { Metric } from "danielbonifacio-sdk";
 
